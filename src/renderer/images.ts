98d77a71780319c72a1c8b6532b3f7797bfd327e
alexanderkershaw <alexander-12345@hotmail.co.uk>
2022-08-25 22:48:48 +0100

    better image handling

--- src/renderer/images.ts
+++ src/renderer/images.ts
@@ -1,127 +1,137 @@
-const getArenaImage = (id: number) => {
-    return require(`../../assets/arena/${id}.jpg`);
+interface imageObject {
+    [id: number]: string;
 }
 
-const getDungeonImage = (id: number) => {
-    return require(`../../assets/dungeon/${id}.jpg`);
+// Load the arena images. This expects files with the name <id>.jpg 
+// to exist in the appropriate assets folder. 
+const arenaIDs = [
+    1672, // Blade's Edge
+    617,  // Dalaran
+    1505, // Nagrand 
+    572,  // Ruins of Lordareon
+    2167, // Robodome
+    1134, // Tigers Peak
+    980,  // Tol'viron
+    1504, // Black Rook
+    2373, // Empyrean Domain
+    1552, // Ashamane's Fall
+    1911, // Mugambala
+    1825, // Hook Point
+    2509, // Maldraxxus
+    2547  // Enigma Crucible
+]
+
+let arena: imageObject = {};
+
+for (const id of arenaIDs) {
+    arena[id] = require(`../../assets/arena/${id}.jpg`);
 }
 
-const getRaidImage = (id: number) => {
-    return require(`../../assets/raid/${id}.jpg`);
+// Load the dungeon images. This expects files with the name <id>.jpg 
+// to exist in the appropriate assets folder.
+const dungeonIDs = [
+    // Shadowlands
+    2293, // Theatre of Pain
+    2441, // Tazavesh
+    2287, // Halls of Atonement
+    2286, // Necrotic Wake
+    2258, // Spires of Ascension
+    2289, // Plaguefall 
+    2284, // Sanguine Depths
+    2291, // De Other Side
+    2290, // Mists of Tirna Sithe
+]
+
+let dungeon: imageObject = {};
+
+for (const id of dungeonIDs) {
+    dungeon[id] = require(`../../assets/dungeon/${id}.jpg`);
 }
 
-const getBattlegroundImage = (id: number) => {
-    return require(`../../assets/battlegrounds/${id}.jpg`);
+// Load the raid encounter images. This expects files with the 
+// name <id>.jpg to exist in the appropriate assets folder.
+const raidIDs = [
+    // Sepulcher of the First Ones
+    // Shadowlands Tier 3
+    2512, // Vigilant Guardian
+    2537, // Jailer
+    2539, // Luhvium
+    2540, // Desaugne
+    2542, // Skolex
+    2543, // Dreadlords
+    2544, // Pantheon
+    2546, // Anduin
+    2549, // Rygelon
+    2553, // Artificer
+    2529, // Halondrus
+]
+
+let raid: imageObject = {};
+
+for (const id of raidIDs) {
+    raid[id] = require(`../../assets/raid/${id}.jpg`);
 }
 
-const getSpecImage = (id: number) => {
-    return require(`../../assets/specs/${id}.png`);
+// Load the battleground images. This expects files with the 
+// name <id>.jpg to exist in the appropriate assets folder.
+
+// Some of these have two entries either because there is 
+// two versions of the map (EOTS casual vs rated) or for no 
+// reason other than I don't have a good exhaustive list. 
+const battlegroundIDs = [
+    2107, // Arathi Basin
+    1681, // Arathi Basin
+    30,   // Alterac Valley
+    761,  // Battle for Gilneas
+    1105, // Deepwind Gorge
+    2245, // Deepwind Gorge
+    566,  // EOTS
+    968,  // EOTS
+    628,  // IOC
+    727,  // Silvershard Mines
+    1803, // Seething Shore
+    998,  // Temple of Kotmogu
+    726,  // Twin Peaks
+    489,  // Warsong Gulch
+]
+
+let battleground: imageObject = {};
+
+for (const id of battlegroundIDs) {
+    battleground[id] = require(`../../assets/battlegrounds/${id}.jpg`);
 }
 
-// Still some more clean-up to do here. Current plan is to have
-// a list of IDs that we loop through and build the objects below.
+// Load the battleground images. This expects files with the name <id>.jpg 
+// to exist in the appropriate assets folder. For reference see: 
+// https://wowpedia.fandom.com/wiki/SpecializationID
+const specIDs = [
+    250, 251, 252,      // Death Knight
+    577, 581,           // Demon Hunter
+    102, 103, 104, 105, // Druid
+    253, 254, 255,      // Hunter
+    62, 63, 64,         // Mage
+    268, 270, 269,      // Monk
+    65, 66, 70,         // Paladin
+    256, 257, 258,      // Priest
+    259, 260, 261,      // Rogue
+    262, 263, 264,      // Shaman
+    265, 266, 267,      // Warlock
+    71, 72, 73          // Warrior
+]
 
-const arena = {
-    1672: getArenaImage(1672),
-    617:  getArenaImage(617),
-    1505: getArenaImage(1505),
-    572:  getArenaImage(572),
-    2167: getArenaImage(2167),
-    1134: getArenaImage(1134), 
-    980:  getArenaImage(980), 
-    1504: getArenaImage(1504), 
-    2373: getArenaImage(2373),
-    1552: getArenaImage(1552), 
-    1911: getArenaImage(1911),
-    1825: getArenaImage(1825),
-    2509: getArenaImage(2509),
-    2547: getArenaImage(2547),
+// Set a 'not found' icon for defaulting to if everything goes wrong.
+// This is also what we default to over upgrade of the application when 
+// previous videos don't have the metadata to show the spec. 
+let spec: imageObject = { 
+    0: require("../../assets/icon/wowNotFound.png") 
+};
+
+for (const id of specIDs) {
+    spec[id] = require(`../../assets/specs/${id}.png`);
 }
 
-const dungeon = {
-    2293: getDungeonImage(2293),
-    2441: getDungeonImage(2441),
-    2287: getDungeonImage(2287),
-    2286: getDungeonImage(2286),
-    2258: getDungeonImage(2258),
-    2289: getDungeonImage(2289),
-    2284: getDungeonImage(2284),
-    2291: getDungeonImage(2291),
-    2290: getDungeonImage(2290),
-}
-
-const raid = {
-    2512: getRaidImage(2512),
-    2537: getRaidImage(2537),
-    2539: getRaidImage(2539),
-    2540: getRaidImage(2540),
-    2542: getRaidImage(2542),
-    2543: getRaidImage(2543),
-    2544: getRaidImage(2544),
-    2546: getRaidImage(2546),
-    2549: getRaidImage(2549),
-    2553: getRaidImage(2553),
-    2529: getRaidImage(2529),
-}
-
-
-const battleground = {
-    2107: getBattlegroundImage(2107),
-    1681: getBattlegroundImage(1681),
-    30:   getBattlegroundImage(30),
-    761:  getBattlegroundImage(761),
-    1105: getBattlegroundImage(1105),
-    2245: getBattlegroundImage(2245),
-    566:  getBattlegroundImage(566),
-    968:  getBattlegroundImage(968),
-    628:  getBattlegroundImage(628),
-    727:  getBattlegroundImage(727),
-    1803: getBattlegroundImage(1803),
-    998:  getBattlegroundImage(998),
-    726:  getBattlegroundImage(726),
-    489:  getBattlegroundImage(489),
-}
-
-const spec = {
-    0: require("../../assets/icon/wowNotFound.png"),
-    250: getSpecImage(250),
-    251: getSpecImage(251),
-    252: getSpecImage(252),
-    577: getSpecImage(577),
-    581: getSpecImage(581),
-    102: getSpecImage(102),
-    103: getSpecImage(103),
-    104: getSpecImage(104),
-    105: getSpecImage(105),
-    253: getSpecImage(253),
-    254: getSpecImage(254),
-    255: getSpecImage(255),
-    62: getSpecImage(62),
-    63: getSpecImage(63),
-    64: getSpecImage(64),
-    268: getSpecImage(268),
-    270: getSpecImage(270),
-    269: getSpecImage(269),
-    65: getSpecImage(65),
-    66: getSpecImage(66),
-    70: getSpecImage(70),
-    256: getSpecImage(256),
-    257: getSpecImage(257),
-    258: getSpecImage(258),
-    259: getSpecImage(259),
-    260: getSpecImage(260),
-    261: getSpecImage(261),
-    262: getSpecImage(262),
-    263: getSpecImage(263),
-    264: getSpecImage(264),
-    265: getSpecImage(265),
-    266: getSpecImage(266),
-    267: getSpecImage(267),
-    71: getSpecImage(71),
-    72: getSpecImage(72),
-    73: getSpecImage(73),
-}
-
+// Finally done.
+// Export everything. 
 export {
     arena,
     dungeon,
