b938409edd020931d8694c350964c10aa26d3271
alexanderkershaw <alexander-12345@hotmail.co.uk>
2022-09-04 09:47:04 +0100

    afk out improvements/test

--- resources/test-scripts/all_tests.py
+++ resources/test-scripts/all_tests.py
@@ -5,7 +5,7 @@ dirname = os.path.dirname(__file__)
 
 tests = {
     "2v2": os.path.join(dirname, "./2v2.py"),
-    "2v2-afk-out": os.path.join(dirname, "./2v2-ahk-out.py"),
+    "2v2-afk-out": os.path.join(dirname, "./2v2-afk-out.py"),
     "3v3": os.path.join(dirname, "./3v3.py"),
     "Skirmish": os.path.join(dirname, "./skirmish.py"),
     "Solo Shuffle": os.path.join(dirname, "./soloshuffle.py"),
