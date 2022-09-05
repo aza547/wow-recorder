dbf256309cd778853915be392f5c1ae189be7944
Chris Sutcliff <chris@sutcliff.me>
2022-09-05 17:43:18 +0100

    replace '@mui/styles' with 'tss-react/mui'
    '@mui/styles' is deprecated, tss-react is recommended to replaces it.

--- src/renderer/Layout.tsx
+++ src/renderer/Layout.tsx
@@ -3,7 +3,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from 'tss-react/mui';
 import { categories, videoTabsSx, categoryTabSx, categoryTabsSx }  from '../main/constants';
 import VideoButton  from './VideoButton';
 
