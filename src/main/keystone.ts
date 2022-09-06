9b39b35a013088c56975557267bfd888228facd1
Thomas Daugaard <daugaard@gmail.com>
2022-09-06 12:07:57 +0200

    Add new classes for Mythic+ support
    Adds `VideoSegmentType` enum, `ChallengeModeVideoSegment` class, and
    `ChallengeModeDungeon` class which together make up the main
    implementation of the support for Mythic Keystone dungeons.

---
+++ src/main/keystone.ts
@@ -0,0 +1,82 @@
+import { dungeonTimersByMapId } from "./constants";
+
+enum VideoSegmentType {
+    BossEncounter = 'Boss',
+    Trash = 'Trash'
+};
+
+class ChallengeModeVideoSegment {
+    logEnd?: Date
+    result?: Boolean;
+
+    constructor(
+        public segmentType: VideoSegmentType,
+        public logStart: Date,
+        public ts: number,
+        public encounterId?: number
+    ) {}
+};
+
+class ChallengeModeDungeon {
+    completed: boolean = false;
+    duration: number = 0;
+    videoSegments: ChallengeModeVideoSegment[] = []
+
+    constructor(
+        public zoneId: number,
+        public mapId: number, // Some dungeons like Karazhan and such have have the
+                              // same zoneId for both Upper/Lower, but have distinct
+                              // mapVersion numbers.
+        public level: number,
+        public affixes: number[]
+    ) {}
+
+    /**
+     * Add a video segment, optionally ending a current one
+     *
+     * Given a date as endPrevious, that date is used as the logEnd for the
+     * current segment.
+     */
+    addVideoSegment(segment: ChallengeModeVideoSegment, endPrevious?: Date) {
+        if (endPrevious) {
+            this.endVideoSegment(endPrevious);
+        }
+
+        this.videoSegments.push(segment);
+    }
+
+    getCurrentVideoSegment(): ChallengeModeVideoSegment | undefined {
+        return this.videoSegments.at(-1)
+    }
+
+    endVideoSegment(logDate: Date) {
+        const currentSegment = this.getCurrentVideoSegment()
+        if (currentSegment) {
+            currentSegment.logEnd = logDate
+        }
+    }
+
+    // Remove the last video segment
+    removeLastSegment() {
+        this.videoSegments.pop();
+    }
+};
+
+const calculateCompletionResult = (mapId: number, duration: number): number => {
+    const timerValues = dungeonTimersByMapId[mapId]
+
+    for (let i = timerValues.length - 1; i >= 0; i--) {
+        if (duration <= timerValues[i]) {
+            return i + 1;
+        }
+    }
+
+    return 0;
+}
+
+export {
+    VideoSegmentType,
+    ChallengeModeVideoSegment,
+    ChallengeModeDungeon,
+    calculateCompletionResult,
+}
