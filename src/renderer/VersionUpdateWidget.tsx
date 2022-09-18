593e1b41b610094f4083d4c573ade559b15ef517
NickIs211 <nick.isom.211@gmail.com>
2022-09-18 10:55:15 -0500

    fixed a few lint errors

--- src/renderer/VersionUpdateWidget.tsx
+++ src/renderer/VersionUpdateWidget.tsx
@@ -3,24 +3,23 @@ import React from 'react';
 const ipc = window.electron.ipcRenderer;
 
 export default function VersionUpdateWidget() {
-
   const openReleaseDownloadUrl = () => {
     ipc.sendMessage('openURL', [downloadUrl]);
   };
 
-  const [widgetClass, setWidgetClass] = React.useState("hidden");
-  const [downloadUrl, setDownloadUrl] = React.useState<string>("");
+  const [widgetClass, setWidgetClass] = React.useState('hidden');
+  const [downloadUrl, setDownloadUrl] = React.useState<string>('');
 
   React.useEffect(() => {
-    window.electron.ipcRenderer.on('updateAvailable', (downloadUrl) => {
+    ipc.on('updateAvailable', (downloadUrl) => {
       setWidgetClass('');
       setDownloadUrl(downloadUrl as string);
     });
   }, []);
 
   return (
-    <div className={"version-update-widget " + widgetClass}>
-      <div className="">
+    <div className={`version-update-widget ${widgetClass}`}>
+      <div>
         <a href="#" onClick={openReleaseDownloadUrl}>
           New update available! Click here to download.
         </a>
