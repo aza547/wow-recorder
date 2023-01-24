32a82b24b782aead452c79b410acc49d02eea1cf
alexanderkershaw <alexander-12345@hotmail.co.uk>
2023-01-24 21:12:33 +0000

    tweaks

--- src/__tests__/activitys/ArenaMatch.test.ts
+++ src/__tests__/activitys/ArenaMatch.test.ts
@@ -1,17 +1,17 @@
-import ArenaMatch from '../activitys/ArenaMatch';
-import { Flavour } from '../main/types';
-import { VideoCategory } from '../types/VideoCategory';
-import Combatant from '../main/Combatant';
+import ArenaMatch from '../../activitys/ArenaMatch';
+import { Flavour } from '../../main/types';
+import { VideoCategory } from '../../types/VideoCategory';
+import Combatant from '../../main/Combatant';
 
-test('Basic Retail 2v2', () => {
+test('Basic Arena Match', () => {
+  const startDate = new Date('2022-12-25T12:00:00');
+
   const testCombatants = [
     new Combatant('Player-1329-09C34603', 0, 253),
     new Combatant('Player-1084-08A89569', 0, 256),
     new Combatant('Player-1092-0A70E103', 1, 557),
     new Combatant('Player-5810-0A3E1BD5', 1, 105),
   ];
-
-  const startDate = new Date('2022-12-25T12:00:00');
 
   const arenaMatch = new ArenaMatch(
     startDate,
@@ -22,12 +22,12 @@ test('Basic Retail 2v2', () => {
 
   arenaMatch.playerGUID = testCombatants[0].GUID;
 
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < testCombatants.length; i++) {
     arenaMatch.addCombatant(testCombatants[i]);
   }
 
-  const endDate = new Date('2022-12-25T12:05:00');
   const winningTeamID = 0;
+  const endDate = new Date('2022-12-25T12:05:00');
   arenaMatch.endArena(endDate, winningTeamID);
 
   const expectedDuration = (endDate.getTime() - startDate.getTime()) / 1000;
@@ -35,4 +35,7 @@ test('Basic Retail 2v2', () => {
 
   expect(arenaMatch.duration).toBe(expectedDuration);
   expect(arenaMatch.result).toBe(expectedResult);
+  expect(arenaMatch.resultInfo).toBe('Win');
+  expect(arenaMatch.zoneName).toBe("Blade's Edge");
+  expect(arenaMatch.getFileName()).toBe("2v2 Blade's Edge (Win)");
 });
