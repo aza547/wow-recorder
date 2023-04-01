5951dcf0e85ff1db3c0a8c709f058f8baf44ec1f
alexanderkershaw <alexander-12345@hotmail.co.uk>
2023-04-01 23:25:23 +0100

    fallback image

--- src/renderer/HomePage.tsx
+++ src/renderer/HomePage.tsx
@@ -3,6 +3,7 @@ import { VideoCategory } from 'types/VideoCategory';
 import { TNavigatorState } from 'main/types';
 import React from 'react';
 import icon from '../../assets/icon/large-icon.png';
+import poster from '../../assets/poster/poster.png';
 import { getLatestCategory, getNumVideos } from './rendererutils';
 
 interface IProps {
@@ -13,6 +14,8 @@ interface IProps {
 const HomePage: React.FC<IProps> = (props: IProps) => {
   const { videoState, setNavigation } = props;
   const latestCategory = getLatestCategory(videoState);
+  const numVideos = getNumVideos(videoState);
+  const haveVideos = numVideos > 0;
 
   let thumbnailPath: string | undefined;
 
@@ -30,6 +33,66 @@ const HomePage: React.FC<IProps> = (props: IProps) => {
       categoryIndex,
       videoIndex: 0,
     });
+  };
+
+  const setFallbackImage = (event: any) => {
+    event.target.src = poster;
+  };
+
+  const renderLatestVideo = () => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            width: '50%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <Box
+            component="img"
+            src={thumbnailPath}
+            onError={setFallbackImage}
+            onClick={goToLatestVideo}
+            sx={{
+              border: '1px solid white',
+              borderRadius: 0,
+              boxSizing: 'border-box',
+              display: 'flex',
+              height: '100%',
+              width: '100%',
+              objectFit: 'cover',
+              '&:hover': {
+                border: '1px solid #bb4420',
+                color: 'gray',
+                backgroundColor: 'lightblue',
+              },
+            }}
+          />
+          <Typography
+            align="center"
+            variant="h5"
+            sx={{
+              color: 'white',
+              fontFamily: '"Arial",sans-serif',
+            }}
+          >
+            Latest Video
+          </Typography>
+        </Box>
+      </Box>
+    );
   };
 
   return (
@@ -68,59 +131,10 @@ const HomePage: React.FC<IProps> = (props: IProps) => {
           fontFamily: '"Arial",sans-serif',
         }}
       >
-        You have {getNumVideos(videoState)} videos saved.
+        You have {numVideos} videos saved.
       </Typography>
 
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'row',
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            width: '50%',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-          }}
-        >
-          <Box
-            component="img"
-            src={thumbnailPath}
-            onClick={goToLatestVideo}
-            sx={{
-              border: '1px solid white',
-              borderRadius: 0,
-              boxSizing: 'border-box',
-              display: 'flex',
-              height: '100%',
-              width: '100%',
-              objectFit: 'cover',
-              '&:hover': {
-                border: '1px solid #bb4420',
-                color: 'gray',
-                backgroundColor: 'lightblue',
-              },
-            }}
-          />
-          <Typography
-            align="center"
-            variant="h5"
-            sx={{
-              color: 'white',
-              fontFamily: '"Arial",sans-serif',
-            }}
-          >
-            Latest Video
-          </Typography>
-        </Box>
-      </Box>
+      {haveVideos && renderLatestVideo()}
     </Box>
   );
 };
--- src/renderer/rendererutils.ts
+++ src/renderer/rendererutils.ts
@@ -177,7 +177,7 @@ const getLatestCategory = (videoState: any) => {
       latestCategory = category as VideoCategory;
     }
   });
-  
+
   return latestCategory;
 };
 
