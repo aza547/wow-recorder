1b4a5a3d97559dac8177b94d7b3afbf7a1c77360
aza547 <alexander-12345@hotmail.co.uk>
2025-01-01 14:43:08 +0000

    fix tests

--- src/__tests__/parser/Basic.test.ts
+++ src/__tests__/parser/Basic.test.ts
@@ -19,7 +19,7 @@ test('Basic Retail', async () => {
   combatLogParser.handleLogLine(arenaMatchStartLine);
 
   const testLogLine = await testLogLinePromise;
-  const expectedDate = new Date('2024-08-03T22:12:04');
+  const expectedDate = new Date('2025-08-03T22:12:04');
 
   expect(testLogLine.date()).toStrictEqual(expectedDate);
   expect(testLogLine.type()).toBe('ARENA_MATCH_START');
@@ -34,18 +34,18 @@ test('Date Parsing', async () => {
   // Pre The War Within expansion there were note years or timezones.
   const preTWW = '8/3 22:12:04.000  ARENA_MATCH_START,2547,33,5v5,1';
   const parsedPreTWW = new LogLine(preTWW);
-  const expectedPreTww = new Date('2024-08-03T22:12:04');
+  const expectedPreTww = new Date('2025-08-03T22:12:04');
   expect(parsedPreTWW.date()).toStrictEqual(expectedPreTww);
 
   // Year but no TZ.
-  const twwNoTZ = '7/28/2024 17:35:43.4931  ARENA_MATCH_START,2547,33,5v5,1';
+  const twwNoTZ = '7/28/2025 17:35:43.4931  ARENA_MATCH_START,2547,33,5v5,1';
   const parsedTwwNoTZ = new LogLine(twwNoTZ);
-  const expectedTwwNoTZ = new Date('2024-07-28T17:35:43');
+  const expectedTwwNoTZ = new Date('2025-07-28T17:35:43');
   expect(parsedTwwNoTZ.date()).toStrictEqual(expectedTwwNoTZ);
 
   // Year and TZ.
-  const twwTz = '7/28/2024 14:37:54.790-4  ARENA_MATCH_START,2547,33,5v5,1';
+  const twwTz = '7/28/2025 14:37:54.790-4  ARENA_MATCH_START,2547,33,5v5,1';
   const parsedTwwTz = new LogLine(twwTz);
-  const expectedTwwTz = new Date('2024-07-28T14:37:54');
+  const expectedTwwTz = new Date('2025-07-28T14:37:54');
   expect(parsedTwwTz.date()).toStrictEqual(expectedTwwTz);
 });
