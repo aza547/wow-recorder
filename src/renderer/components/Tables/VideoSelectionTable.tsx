26b1bbb4ca33f7915cc1f8da88aa4edfe4055e0b
aza547 <alexander-12345@hotmail.co.uk>
2025-02-14 13:16:39 +0000

    ctrl and shift selection in video table

--- src/renderer/components/Tables/VideoSelectionTable.tsx
+++ src/renderer/components/Tables/VideoSelectionTable.tsx
@@ -58,7 +58,7 @@ const VideoSelectionTable = (props: IProps) => {
       if (event.key === 'Shift') setShiftDown(true);
     });
 
-    // Prevent shift mouse down selecting all the text in the table.
+    // Prevent shift / ctrl mouse down selecting all the text in the table.
     document.addEventListener('mousedown', (event) => {
       if (event.shiftKey) event.preventDefault();
       if (event.ctrlKey) event.preventDefault();
