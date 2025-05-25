05755d07de6ee9de4d2a45bd59f196fa8fcdbcd4
aza547 <alexander-12345@hotmail.co.uk>
2025-05-25 10:21:17 +0100

    add a hide empty categories option

---
+++ src/renderer/ApplicationSettings.tsx
@@ -0,0 +1,142 @@
+import * as React from 'react';
+import { ConfigurationSchema } from 'config/configSchema';
+import { AppState } from 'main/types';
+import { getLocalePhrase, Phrase } from 'localisation/translations';
+import { setConfigValues } from './useSettings';
+import Switch from './components/Switch/Switch';
+import Label from './components/Label/Label';
+import { Dispatch, SetStateAction } from 'react';
+
+interface IProps {
+  appState: AppState;
+  config: ConfigurationSchema;
+  setConfig: Dispatch<SetStateAction<ConfigurationSchema>>;
+}
+
+const ApplicationSettings = (props: IProps) => {
+  const { appState, config, setConfig } = props;
+  const initialRender = React.useRef(true);
+
+  React.useEffect(() => {
+    // Don't fire on the initial render.
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    }
+
+    setConfigValues({
+      startUp: config.startUp,
+      startMinimized: config.startMinimized,
+      minimizeOnQuit: config.minimizeOnQuit,
+      minimizeToTray: config.minimizeToTray,
+      hideEmptyCategories: config.hideEmptyCategories,
+    });
+  }, [
+    config.minimizeOnQuit,
+    config.minimizeToTray,
+    config.startMinimized,
+    config.startUp,
+    config.hideEmptyCategories,
+  ]);
+
+  const getSwitch = (
+    preference: keyof ConfigurationSchema,
+    changeFn: (checked: boolean) => void,
+  ) => (
+    <Switch
+      checked={Boolean(config[preference])}
+      name={preference}
+      onCheckedChange={changeFn}
+    />
+  );
+
+  const getSwitchForm = (
+    preference: keyof ConfigurationSchema,
+    label: Phrase,
+    changeFn: (checked: boolean) => void,
+  ) => {
+    return (
+      <div className="flex flex-col">
+        <Label htmlFor="separateBufferPath">
+          {getLocalePhrase(appState.language, label)}
+        </Label>
+        <div className="flex h-10 items-center">
+          {getSwitch(preference, changeFn)}
+        </div>
+      </div>
+    );
+  };
+
+  const setRunOnStartup = (checked: boolean) => {
+    setConfig((prevState) => {
+      return {
+        ...prevState,
+        startUp: checked,
+      };
+    });
+  };
+
+  const setStartMinimized = (checked: boolean) => {
+    setConfig((prevState) => {
+      return {
+        ...prevState,
+        startMinimized: checked,
+      };
+    });
+  };
+
+  const setMinimizeOnQuit = (checked: boolean) => {
+    setConfig((prevState) => {
+      return {
+        ...prevState,
+        minimizeOnQuit: checked,
+      };
+    });
+  };
+
+  const setMinimizeToTray = (checked: boolean) => {
+    setConfig((prevState) => {
+      return {
+        ...prevState,
+        minimizeToTray: checked,
+      };
+    });
+  };
+
+  const setHideEmptyCategories = (checked: boolean) => {
+    setConfig((prevState) => {
+      return {
+        ...prevState,
+        hideEmptyCategories: checked,
+      };
+    });
+  };
+
+  return (
+    <div className="flex flex-row flex-wrap gap-x-8">
+      {getSwitchForm('startUp', Phrase.RunOnStartupLabel, setRunOnStartup)}
+      {getSwitchForm(
+        'startMinimized',
+        Phrase.StartMinimizedLabel,
+        setStartMinimized,
+      )}
+      {getSwitchForm(
+        'minimizeOnQuit',
+        Phrase.MinimizeOnQuitLabel,
+        setMinimizeOnQuit,
+      )}
+      {getSwitchForm(
+        'minimizeToTray',
+        Phrase.MinimizeToTrayLabel,
+        setMinimizeToTray,
+      )}
+      {getSwitchForm(
+        'hideEmptyCategories',
+        Phrase.HideEmptyCategoriesLabel,
+        setHideEmptyCategories,
+      )}
+    </div>
+  );
+};
+
+export default ApplicationSettings;
