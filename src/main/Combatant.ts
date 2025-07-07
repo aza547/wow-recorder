4e31ea7dc3eddaf355d25455c17ff7dba8273a4a
Vladimir Ulyanov <lionskape@gmail.com>
2025-07-07 23:55:38 +0300

    Add a region from combat log to the saved metadata.

--- src/main/Combatant.ts
+++ src/main/Combatant.ts
@@ -14,6 +14,8 @@ export default class Combatant {
 
   private _realm?: string;
 
+  private _region?: string;
+
   /**
    * Constructs a new Combatant.
    *
@@ -107,13 +109,30 @@ export default class Combatant {
     this._realm = value;
   }
 
+  /**
+   * Gets the region.
+   */
+  get region() {
+    return this._region;
+  }
+
+  /**
+   * Sets the region.
+   */
+  set region(value) {
+    this._region = value;
+  }
+
   isFullyDefined() {
     const hasGUID = this.teamID !== undefined;
     const hasName = this.name !== undefined;
     const hasRealm = this.realm !== undefined;
+    const hasRegion = this.region !== undefined;
     const hasSpecID = this.specID !== undefined;
     const hasTeamID = this.teamID !== undefined;
-    return hasGUID && hasName && hasRealm && hasSpecID && hasTeamID;
+    return (
+      hasGUID && hasName && hasRealm && hasRegion && hasSpecID && hasTeamID
+    );
   }
 
   getRaw(): RawCombatant {
@@ -123,6 +142,7 @@ export default class Combatant {
     if (this.specID !== undefined) rawCombatant._specID = this.specID;
     if (this.name !== undefined) rawCombatant._name = this.name;
     if (this.realm !== undefined) rawCombatant._realm = this.realm;
+    if (this.region !== undefined) rawCombatant._region = this.region;
 
     return rawCombatant;
   }
