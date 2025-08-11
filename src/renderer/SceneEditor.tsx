80d51c34b274888e4b455cff4bba86494930a544
aza547 <alexander-12345@hotmail.co.uk>
2025-08-11 22:35:48 +0100

    reset and preview toggle

--- src/renderer/SceneEditor.tsx
+++ src/renderer/SceneEditor.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppState, RecStatus } from 'main/types';
 import { Phrase } from 'localisation/types';
 import { getLocalePhrase } from 'localisation/translations';
@@ -15,14 +15,58 @@ import {
   TabsList,
   TabsTrigger,
 } from './components/Tabs/Tabs';
+import { Button } from './components/Button/Button';
+import Switch from './components/Switch/Switch';
+import { Tooltip } from './components/Tooltip/Tooltip';
+
+const ipc = window.electron.ipcRenderer;
 
 interface IProps {
   appState: AppState;
   recorderStatus: RecStatus;
 }
 
+type BoxDimensions = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
 const SceneEditor: React.FC<IProps> = (props: IProps) => {
   const { recorderStatus, appState } = props;
+  const [previewEnabled, setPreviewEnabled] = React.useState(true);
+
+  // Kindof hate all this stuff living here and not in RecorderPreview (also might have a perf hit?)
+  // But for now it's needed to enable the reset buttons. Must be a better way though. TODO.
+  const [overlayBoxDimensions, setOverlayDimensions] = useState<BoxDimensions>({
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 100,
+  });
+
+  const [gameBoxDimensions, setGameBoxDimensions] = useState<BoxDimensions>({
+    x: 0,
+    y: 0,
+    width: 1000,
+    height: 500,
+  });
+
+  // TODO: Ditto: surely this can live in the RecorderPreview.
+  const initDraggableBoxes = async () => {
+    const s = await ipc.getSourcePosition('WCR Overlay');
+    setOverlayDimensions(s);
+    const g = await ipc.getSourcePosition('WCR Window Capture');
+    setGameBoxDimensions(g);
+  };
+
+  // TODO: Ditto again.
+  useEffect(() => {
+    // On component mount, get the source dimensions from the backend
+    // to initialize the draggable boxes.
+    initDraggableBoxes();
+  }, []);
 
   return (
     <Box
@@ -36,7 +80,13 @@ const SceneEditor: React.FC<IProps> = (props: IProps) => {
       className="bg-background-higher pt-[32px]"
     >
       <Box sx={{ width: '100%', height: '60%' }}>
-        <RecorderPreview />
+        <RecorderPreview
+          previewEnabled={previewEnabled}
+          gameBoxDimensions={gameBoxDimensions}
+          setGameBoxDimensions={setGameBoxDimensions}
+          overlayBoxDimensions={overlayBoxDimensions}
+          setOverlayDimensions={setOverlayDimensions}
+        />
       </Box>
       <Tabs defaultValue="source" className="w-full h-[40%] px-4">
         <TabsList>
@@ -52,7 +102,50 @@ const SceneEditor: React.FC<IProps> = (props: IProps) => {
           <TabsTrigger value="overlay">
             {getLocalePhrase(appState.language, Phrase.OverlayHeading)}
           </TabsTrigger>
+          <div className="flex ml-auto items-center justify-center gap-x-2">
+            <Button
+              className="flex w-[60px]"
+              variant="ghost"
+              size="xs"
+              onClick={() => {
+                ipc.resetSourcePosition('WCR Window Capture');
+                initDraggableBoxes();
+              }}
+            >
+              <span className="text-xs text-foreground-lighter">
+                Reset
+                <br />
+                Game
+              </span>
+            </Button>
+            <Button
+              className="flex w-[60px]"
+              variant="ghost"
+              size="xs"
+              onClick={() => {
+                ipc.resetSourcePosition('WCR Overlay');
+                initDraggableBoxes();
+              }}
+            >
+              <span className="text-xs text-foreground-lighter">
+                Reset
+                <br />
+                Overlay
+              </span>
+            </Button>
+            <Tooltip content="Preview On">
+              <Box className="flex w-[60px] items-center justify-center">
+                <Switch
+                  checked={previewEnabled}
+                  onCheckedChange={setPreviewEnabled}
+                >
+                  Hide
+                </Switch>
+              </Box>
+            </Tooltip>
+          </div>
         </TabsList>
+
         <ScrollArea
           withScrollIndicators={false}
           className="h-[calc(100%-48px)] pb-8"
