1d850540545207c14f4b8b10f39e4fd2fa16af34
aza547 <alexander-12345@hotmail.co.uk>
2026-06-17 16:55:18 +0100

    auto preload

--- src/renderer/VideoPlayer.tsx
+++ src/renderer/VideoPlayer.tsx
@@ -734,6 +734,7 @@ export const VideoPlayer = forwardRef<VideoPlayerRef, IProps>((props, ref) => {
     return (
       <ReactPlayer
         id="react-player"
+        preload={'auto'}
         ref={player}
         height="100%"
         width="100%"
