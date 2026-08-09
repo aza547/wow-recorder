12aca4f09ac33b6302a8be301b3dcf889387d32e
aza547 <alexander-12345@hotmail.co.uk>
2026-08-09 09:07:09 +0100

    new icons

--- src/renderer/icons/FolderUnlocked.tsx
+++ src/renderer/icons/FolderUnlocked.tsx
@@ -16,7 +16,7 @@ export function FolderUnlocked({ size = 24, ...props }: LucideProps) {
     >
       <path d="M10 20H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3.9a2 2 0 0 1 1.69.9l.81 1.2a2 2 0 0 0 1.67.9H20a2 2 0 0 1 1" />
       <rect x="13.5" y="15.5" width="10" height="5.5" rx="1" />
-      <path d="M15.5 15.5v-1.75a2.5 2.5 0 0 1 4.5-1.5" />
+      <path d="M16 15.0v-1.75a2.5 2.5 0 0 1 4.5-1.5" />
     </svg>
   );
 }
