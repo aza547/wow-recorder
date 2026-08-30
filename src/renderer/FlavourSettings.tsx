748c27a2e16844e4735b6d56d9cc8183a69f8528
aza547 <alexander-12345@hotmail.co.uk>
2026-08-30 21:02:50 +0100

    tidy

--- src/renderer/FlavourSettings.tsx
+++ src/renderer/FlavourSettings.tsx
@@ -29,11 +29,13 @@ const FlavourSettings: React.FC<IProps> = (props: IProps) => {
   const initialRender = React.useRef(true);
 
   React.useEffect(() => {
+    // Refresh the combat log status on the initial render of this component.
+    // There is no mechanism to refresh that live, unlike the advanced combat
+    // logging status because I can't be bothered to implement that.
+    ipc.refreshCombatLogStatus();
+
     if (initialRender.current) {
-      // Refresh the combat log status on the initial render of this component.
-      // There is no mechanism to refresh that live, unlike the advanced combat
-      // logging status because I can't be bothered to implement that.
-      ipc.refreshCombatLogStatus();
+      // Don't change config on the initial render.
       initialRender.current = false;
       return;
     }
