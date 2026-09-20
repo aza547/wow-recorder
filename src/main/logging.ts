5ff44c93381e3774a5334b66125a2bfb01846d51
aza547 <alexander-12345@hotmail.co.uk>
2026-09-21 00:41:10 +0100

    comments, tidy

--- src/main/logging.ts
+++ src/main/logging.ts
@@ -3,6 +3,9 @@ import { fixPathWhenPackaged } from './util';
 import log from 'electron-log/main';
 import fs from 'fs';
 
+/**
+ * Returns a local date string in the format YYYY-MM-DD.
+ */
 const getLocalDate = () => {
   const date = new Date();
 
@@ -13,10 +16,19 @@ const getLocalDate = () => {
   ].join('-');
 };
 
-const logPrefix = 'WarcraftRecorder-';
+const logPrefix = 'WarcraftRecorder';
 let logIndex = 0;
 let logDate = getLocalDate();
 
+/**
+ * Get the next log index on startup of the application as logIndex in memory
+ * does not persist across restarts. Always selects one more than the max
+ * existing index, even if there are gaps.
+ *
+ * This also means that we will rotate to a new log on each startup of the app,
+ * meaning we can rotate the log before it reaches the maximum size, but that's
+ * actually a nice feature.
+ */
 const getNextLogIndex = () => {
   const dir = getApplicationLogDir();
   logDate = getLocalDate();
@@ -24,7 +36,7 @@ const getNextLogIndex = () => {
   try {
     // We only ever run this on startup so it's OK this is not async.
     const files = fs.readdirSync(dir);
-    const prefix = `${logPrefix}${logDate}.`;
+    const prefix = `${logPrefix}-${logDate}.`;
 
     const indices = files
       .filter((file) => file.startsWith(prefix) && file.endsWith('.log'))
@@ -34,6 +46,9 @@ const getNextLogIndex = () => {
     logIndex = indices.length > 0 ? Math.max(...indices) + 1 : 0;
     console.info('[Logging] Application log index is', logIndex);
   } catch {
+    // This log is early such that won't actually appear in the log files, but
+    // will appear in a terminal if attached. It's unlikely to go wrong (unless
+    // the filesystem is broken).
     console.error('[Logging] Failed to read application log directory', dir);
   }
 };
@@ -44,11 +59,12 @@ const getApplicationLogPath = () => {
 
   if (date !== logDate) {
     // Reset the rotation index if the date changed.
+    console.info('[Logging] Date changed, resetting log index.');
     logIndex = 0;
     logDate = date;
   }
 
-  const fileName = `${logPrefix}${date}.${logIndex}.log`;
+  const fileName = `${logPrefix}-${date}.${logIndex}.log`;
   return path.join(dir, fileName);
 };
 
